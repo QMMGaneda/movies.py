7543eb79ebcb9579b2a710d55c24cc419e0982e9
QMMGaneda <142982225+QMMGaneda@users.noreply.github.com>
2023-10-18 08:55:30 +0000

    Add files via upload

--- movies.py
+++ movies.py
@@ -1,19 +1,31 @@
 from flask import Flask, jsonify, request
 app = Flask(__name__)
-movies = (
+movies = [
     {
         "name":"Iron Man",
-        "casts": ["Tony Stark", "Happy"],
-        "genre": ["Action", "Sci Fi"]
+        "casts":["Tony Stark", "Happy", "Jarvis", "Pepper Pots", "Iron Monger", "Rodey"],
+        "genre":["Action", "Sci Fi", "Adventure", "Triller"]
+    },
+    {
+        "name":"Iron Man 2",
+        "casts":["Tony Stark", "Happy", "Jarvis", "Pepper Pots", "Rodey", "Whiplash", "Natasha Romanoff"],
+        "genre":["Action", "Sci Fi", "Adventure", "Triller"]
     }
-)
+]
 @app.route('/movies', methods=['Get'])
 def getMovies():
     return jsonify(movies)
-@app.route('/movies',methods=['POST'])
+
+@app.route('/movies', methods=['POST'])
 def add_movie():
-    movies = request.get_json()
+    movie = request.get_json()
     movies.append(movie)
-    return{}
-if __name__=='__main__':
+    return {'id': len(movies)}, 200
+
+@app.route('/movies/<int:index>', methods=['DELETE'])
+def delete_movie(index):
+    movies.pop(index)
+    return "The movie has been deleted", 200
+
+if __name__ == '__main__':
     app.run()
